0b9c62cc3b2a14ba1279494c42c848d528cbaaca
Anderson Rodrigues <andersonrodrigues.silva96@gmail.com>
2023-03-18 09:47:11 -0300

    feat: added resolution of exercises from week 2

---
+++ week-2/hello-world.js
@@ -0,0 +1,11 @@
+// Exercise 1
+console.log("Meu primeiro arquivo javascript no node!");
+
+// Exercise 2
+let num1 = 2;
+let num2 = 2;
+
+const sum = () => {
+  return num1 + num2;
+};
+console.log(`O resultado da soma é: ${sum()}`);
